8bfa586a2eb5650176b150f0284b7d1f79ade660
B. Thomas Goodwin <bt.goodwin@me.com>
2023-01-20 20:01:24 -0500

    gmmk-pro-v1: drop alt-like 65% macOS configuration
    Number row and right-side buttons mirror the layout of my
    Drop ALT 65% keyboard, bottom row as well follows the
    macOS layout.  RGB heatmap is enabled as the default
    with Fn-R being the toggle to switch modes and
    Fn-Print Screen to toggle RGB on/off.  Also made the Fn-`
    be Escape, like my 65% row.
    
    Signed-off-by: B. Thomas Goodwin <bt.goodwin@me.com>

---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/btgoodwin/rules.mk
@@ -0,0 +1,8 @@
+ENCODER_MAP_ENABLE = yes    # Enable encoder to control volume
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes
+
+MOUSEKEY_ENABLE = no		# Disabling, don't care to have this.
+
+VIA_ENABLE = no
